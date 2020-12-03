eb8b7bc60e9d8b6e44bed624fa95c969624f5c8e
Kelvin <kelvin_trang@yahoo.com>
2020-12-03 15:19:57 -0500

    mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require("body-parser");
+var Experience = require("./models/experience");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,5 +19,20 @@ app.use(express.static(path.join(__dirname, 'public'), {extensions: 'html'}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.post("/create", function(req, res) {
+    var xp = new Experience({
+        company:   req.body.company,
+        title:     req.body.title,
+        location:  req.body.location,
+        startDate: req.body.startDate,
+        endDate:   req.body.endDate,
+    });
+
+    xp.save(function(err, xp) {
+        res.send("Experience with " + xp.id + " was saved.");  
+        console.log(xp);
+     });  
+  });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,32 +1,7 @@
 var mongoose = require("mongoose");
-var express = require("express");
-mongoose.connect("mongodb+srv://testUser:testPassword@cluster0.kabd5.mongodb.net/profileDB?retryWrites=true&w=majority");
-
-var xpSchema = new mongoose.Schema({
-    company:   String,
-    title:     String,
-    location:  String,
-    startDate: String,
-    endDate:   String,
- });
-
- var Experience = mongoose.model("Experience", xpSchema);
-
- var app = express();
-
- app.get("/create", function(req, res) {
-    var xp = new Experience({
-        company:   "Penn State Campus Recreation",
-        title:     "Head-Lifeguard",
-        location:  "University Park, PA",
-        startDate: "Sept 2018",
-        endDate:   "Nov 2019",
-    });
-
-    stu.save(function(err, stu) {
-        res.send("Experience with " + xp.id + " was saved.");  
-     });  
-  });
-
-  app.listen(3000);
+mongoose.connect("mongodb+srv://testUser:testPassword@cluster0.kabd5.mongodb.net/profileDB?retryWrites=true&w=majority", {useNewUrlParser: true},
+{server: {
+    ssl:        false,
+    sslValidate:false
+}});
 module.exports = mongoose;
