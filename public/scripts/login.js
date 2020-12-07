e31e5544a9e7699ffcc32184a100e6915770b47b
Liz <ejm5763@gmail.com>
2020-12-06 23:38:22 -0500

    login.js additions

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,1 +1,37 @@
-//did i need to make this i dont think i did delete if not needed
+
+//when submit thing is clicked do validateAccount function
+document.querySelector("#submit").addEventListener("click", validateAccount);
+
+
+
+
+//create currentUser object
+var currentUser = {username:"kmt5720", password:"kmt5720pass"};
+
+function validateAccount(){
+    if(currentUser.username == "username" && currentUser.password == "password"){
+        location.href = "profile.html";
+    }
+    else if(currentUser.name == "username" && currentUser.password != "password"){
+        console.log("Sorry, but your password is invalid.")
+    }
+    else if(currentUser.name != "username" && currentUser.password == "password"){
+        console.log("Sorry, but your username is invalid.")
+    }
+    else{
+        console.log("Sorry, but your username or password is invalid.")
+    }
+}
+
+
+
+/*idk if i need this lol
+$.ajax({
+    method: 'GET',
+    url: 'profile/get?username=' + username + '&password' + password,
+    success: function(data){
+        
+    }
+})
+
+*/
