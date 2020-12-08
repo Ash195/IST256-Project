3be71f83a5a7529460ab4e76666a27560bec0cc8
weegiraffegravity <Caroline@Carolines-MacBook-Pro-4.local>
2020-12-07 20:18:41 -0500

    some comments

--- public/scripts/connections.js
+++ public/scripts/connections.js
@@ -1,6 +1,46 @@
+//scrapped data for connections and companies
+
 // const rootURL = 'http://api.dataatwork.org/v1/';
 
-// var companiesJSON = '{"companyName": "BAE", "jobs": "Systems Engineer", companyPic: "./images/baeSystems.png}'
+// var companiesJSON = '{ "companies": [
+//     {
+//         "companyName": "bae",
+//         "jobs":["accountant", "linux systems administrator", "computer data analyst"],
+//         "skillFocus":["Linux", "Java", "Ruby"],
+//         "logo": "./images/bae.png"
+//     },
+//     {
+//         "companyName": "ubisoft",
+//         "jobs":["systems engineer"],
+//         "skillFocus":["C++", "Ruby"],
+//         "logo": "./images/Ubisoft.png"
+//     },
+//     {
+//         "companyName": "google",
+//         "jobs":["computer science internships", "front end web developer", "systems administrator", "project manager"],
+//         "skillFocus":["C#", "Java", "CSS", "HTML", "JavaScript"],
+//         "logo": "./images/google.jpg"
+//     },
+//     {
+//         "companyName": "adobe",
+//         "jobs":["data analyst", "site developer", "site designer"],
+//         "skillFocus":["CSS", "JavaScript", "HTML", "SQL"],
+//         "logo": "./images/adobe.png"
+//     },
+//     {
+//         "companyName": "intel",
+//         "jobs":["cyber security internship","cyber security department team member"],
+//         "skillFocus":["C++", "Java", "Python","Ruby"],
+//         "logo": "./images/intel.png"
+//     },
+//     {
+//         "companyName": "honda",
+//         "jobs":["interface designer","application development team"],
+//         "skillFocus":["CSS", "JavaScript", "HTML"],
+//         "logo": "./images/honda.png"
+//     }
+// ]
+// }
 
-    
 
+// let companies = JSON.parse(companiesJSON);
